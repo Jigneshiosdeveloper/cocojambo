07abfa40b34c8535f2fcd58f9bb1a10048e9ed25
Jignesh Patel <jig.86.12@gmail.com>
2023-02-16 18:17:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:coco_jambo_admin/ui/login.dart';
+import 'package:coco_jambo_admin/utils/color.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coco Jambo Admin',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+           primary: const Color(0xFF1B586B),
+        ),
+      ),
+      home: const Login(),
+    );
+  }
+}
+
+
